c72bf2afdde421b3ad1af687e69e326ed98edb8a
Yuva <yuva@codemancers.com>
2018-09-10 18:26:56 +0530

    change env for redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: "sidekiq" }
+  config.redis = { namespace: "sidekiq", url: ENV.fetch("TRAVIS_BAE_REDIS_URL") }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: "sidekiq" }
+  config.redis = { namespace: "sidekiq", url: ENV.fetch("TRAVIS_BAE_REDIS_URL") }
 end
